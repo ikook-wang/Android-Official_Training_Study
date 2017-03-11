8f9c9099057cd9437e2caf0f3e42cb2fe47d8bad
ikook <935542673@qq.com>
2017-03-11 23:18:37 +0800

    Update MyFirstApp

--- MyFirstApp/app/src/main/java/com/ikook/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/ikook/myfirstapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.ikook.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.ikook.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** 当用户单击 Send 按钮时调用* */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
